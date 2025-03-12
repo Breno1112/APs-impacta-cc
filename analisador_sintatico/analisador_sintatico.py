5af3af503383570aa9ff9ff82513157c7498a8ab
Breno1112 <contato.brenofachini@hotmail.com>
2025-03-12 09:12:15 -0300

    analisador sintático da palavra program

--- analisador_sintatico/analisador_sintatico.py
+++ analisador_sintatico/analisador_sintatico.py
@@ -8,11 +8,40 @@ class AnalisadorSintatico:
         self.analisador_lexico = analisador_lexico.AnalisadorLexico(arquivo.read())
         arquivo.close()
 
+        self.quantidade_begins = 0
+        self.quantidade_ends = 0
+        self.atomo_atual = None
+
+
     def analisar(self):
-        atomo = self.analisador_lexico.proximo_atomo()
-        while atomo is not None:
-            if atomo.get('error') is not None:
-                raise RuntimeError(atomo['error'])
-            print('Linha: {} - Atomo: {}    lexema: {}'.format(atomo['linha'], atomo['atomo'], atomo['lexema']))
-            atomo = self.analisador_lexico.proximo_atomo()
+        self.pegar_proximo_atomo()
+        while self.atomo_atual is not None:
+            if self.atomo_atual.get('error') is not None:
+                raise RuntimeError(self.atomo_atual['error'])
+            self.verificar_e_direcionar_atomo()
+            self.pegar_proximo_atomo()
         print('{} linhas analisadas, programa sintaticamente correto.'.format(self.analisador_lexico.last_line()))
+    
+    def imprime_atomo_atual(self):
+        print('Linha: {} - Atomo: {}    lexema: {}'.format(self.atomo_atual['linha'], self.atomo_atual['atomo'], self.atomo_atual['lexema']))
+    
+    def verificar_e_direcionar_atomo(self):
+        if self.atomo_atual['atomo'] == 'PROGRAM':
+            self.analisar_palavra_reservada_program()
+        else:
+            self.imprime_atomo_atual()
+    
+    def pegar_proximo_atomo(self):
+        self.atomo_atual = self.analisador_lexico.proximo_atomo()
+
+    def verificar_e_direcionar_e_validar_proximo_atomo(self, proximo_atomo_esperado):
+        self.pegar_proximo_atomo()
+        if self.atomo_atual['atomo'] != proximo_atomo_esperado:
+            raise RuntimeError('Erro sintático: Esperado [{}] encontrado [{}] na linha {}'.format(proximo_atomo_esperado, self.atomo_atual['atomo'], self.atomo_atual['linha']))
+        else:
+            self.imprime_atomo_atual()
+
+    def analisar_palavra_reservada_program(self):
+        self.imprime_atomo_atual()
+        self.verificar_e_direcionar_e_validar_proximo_atomo('IDENTIF')
+        
