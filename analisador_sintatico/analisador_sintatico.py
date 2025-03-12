bb1a9f2cfdf7f2b07f077af6c0d480dd75ed614b
Eduardo Diamandis <72038107+eduardodiamandis@users.noreply.github.com>
2025-03-12 00:36:39 -0300

    Função para validar Begin | end
    Utiliza um contador que verifica se o número de begin's é o mesmo que o número de end' s

--- analisador_sintatico/analisador_sintatico.py
+++ analisador_sintatico/analisador_sintatico.py
@@ -18,4 +18,17 @@ class AnalisadorSintatico:
         print('{} linhas analisadas, programa sintaticamente correto.'.format(self.analisador_lexico.last_line()))
 
 
-#teste
+    def begin_end(self):
+    #Validar Tokens
+    #A função analisar consome todos os atomos antes de begin_end ser chamada, deixando o analisador léxico sem mais atomos para processar.
+        begin_count = 0
+        end_count = 0
+        atomo = self.analisador_lexico.proximo_atomo()
+        while atomo is not None:
+            if atomo['atomo'] == 'begin':
+                begin_count += 1
+            elif atomo['atomo'] == 'end':
+                end_count += 1
+            atomo = self.analisador_lexico.proximo_atomo()
+        if begin_count != end_count:
+            raise RuntimeError('Erro sintático: quantidade de begin e end diferentes.')
